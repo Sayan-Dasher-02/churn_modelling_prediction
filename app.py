9138592e9198e8409ee1208798716984661a6153
Sayan Mondal <138865443+Sayan-Dasher-02@users.noreply.github.com>
2026-02-25 15:12:50 +0530

    I uploaded the chunk modelling prediction code.

---
+++ app.py
@@ -0,0 +1,57 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import StandardScaler, LabelEncoder, OneHotEncoder
+import tensorflow as tf
+import pickle
+
+model = tf.keras.models.load_model('model.h5')
+
+with open('label_encoder_gender.pkl', 'rb') as file:
+    lebel_encoder_gender = pickle.load(file)
+
+with open('onehot_encoder_geo.pkl', 'rb') as file:
+    onehot_encoder_geo = pickle.load(file)
+
+with open('scaler.pkl', 'rb') as file:
+    scaler = pickle.load(file)
+
+st.title("Customer Churn Prediction")
+
+geography = st.selectbox("Geography", onehot_encoder_geo.categories_[0])
+gender = st.selectbox("Gender", lebel_encoder_gender.classes_)
+age = st.slider("Age", 18, 95)
+tenure = st.slider("Tenure", 0, 10)
+credit_score = st.number_input("Credit Score")
+estimated_salary = st.number_input("Estimated Salary")
+no_of_products = st.slider("Number of Products", 1, 4)
+has_cr_card = st.selectbox("Has Credit Card", [0, 1])
+is_active_member = st.selectbox("Is Active Member", [0, 1])
+balance = st.number_input("Balance")
+
+input_data = pd.DataFrame({
+    'CreditScore': [credit_score],
+    'Gender': [lebel_encoder_gender.transform([gender])[0]],
+    'Age': [age],
+    'Tenure': [tenure],
+    'Balance': [balance],
+    'NumOfProducts': [no_of_products],
+    'HasCrCard': [has_cr_card],
+    'IsActiveMember': [is_active_member],
+    'EstimatedSalary': [estimated_salary],
+})
+
+geo_encoded = onehot_encoder_geo.transform([[geography]]).toarray()
+geo_encoded_df = pd.DataFrame(geo_encoded, columns=onehot_encoder_geo.get_feature_names_out(['Geography']))
+
+input_data = pd.concat([input_data.reset_index(drop=True), geo_encoded_df], axis=1)
+input_data_scaled = scaler.transform(input_data)
+
+prediction = model.predict(input_data_scaled)
+prediction_proba = prediction[0][0]
+if st.button("Predict"):
+    st.write(f"Churn Probability: {prediction_proba: 4f}")
+    if prediction_proba > 0.5:
+        st.write("The customer is likely to churn.")
+    else:
+        st.write("The customer is unlikely to churn.")
